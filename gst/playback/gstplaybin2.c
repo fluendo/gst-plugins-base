82e48401f1e16e5161a5681885f00d50addddf30
Sebastian Dröge <sebastian.droege@collabora.co.uk>
2012-06-14 09:34:08 +0200

    playbin2: Fix documentation of new property

--- gst/playback/gstplaybin2.c
+++ gst/playback/gstplaybin2.c
@@ -862,7 +862,7 @@ gst_play_bin_class_init (GstPlayBinClass * klass)
           G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   /**
-   * GstQueue2:ring-buffer-max-size
+   * GstPlayBin2::ring-buffer-max-size
    *
    * The maximum size of the ring buffer in bytes. If set to 0, the ring
    * buffer is disabled. Default 0.
@@ -877,7 +877,7 @@ gst_play_bin_class_init (GstPlayBinClass * klass)
           G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   /**
-   * GstPlaySink::force-aspect-ratio:
+   * GstPlayBin2::force-aspect-ratio:
    *
    * Requests the video sink to enforce the video display aspect ratio.
    *
