fd0a6976c9dc7e5e77cca3ff3270f46b3c5ea511
Aleksandr Slobodeniuk <aslobodeniuk@fluendo.com>
2019-09-19 10:17:56 +0200

    fx5

--- gst-libs/gst/audio/gstringbuffer.c
+++ gst-libs/gst/audio/gstringbuffer.c
@@ -2056,6 +2056,12 @@ gst_ring_buffer_advance (GstRingBuffer * buf, guint advance)
 
   /* update counter */
   g_atomic_int_add (&buf->segdone, advance);
+
+  if (buf->segdone == g_atomic_int_get (&buf->segtodo)) {
+    GST_DEBUG ("slowing down reading for 20ms to let ringbuffer accumulate data");
+    g_usleep (20 * 1000);
+  }
+
   gst_ring_buffer_signal_waiter (buf);
 }
 
